c6d66d757a8c24e7bed1fd9784baef4341c0505b
Andrea Bragante <andrea.bragante00@gmail.com>
2025-02-26 15:23:00 +0100

    changed arg parse method

--- src/model/ae_main.py
+++ src/model/ae_main.py
@@ -24,18 +24,21 @@ def main(argv):
     print('launching training...')
 
     parser = argparse.ArgumentParser("SAE model")
-    parser.add_argument("--learning_rate", dest='config.learning_rate', type=float, default=0.0001)
-    parser.add_argument("--latent_dim", dest='config.latent_dim', type=int, default=30)
-    parser.add_argument("--batch_size", dest='config.batch_size', type=int, default=30)
-    parser.add_argument("--num_epochs", dest='config.num_epochs', type=int, default=15)
-    parser.add_argument("--roi_class", dest='config.roi_class', default='floc-bodies') # floc-bodies, ...
-    parser.add_argument("--hem", dest='config.hem', default='lh') # lh, rh, all
-    # parser.add_argument("--ds", dest='config.ds', default='fmri') # mnist, cifar10, fmri
-    parser.add_argument("--sparsity", dest='config.sparsity', type=float, default=0.8)
-    parser.add_argument("--l1", dest='config.l1', type=float, default=0.1)
-    parser.add_argument("--subject", dest='config.subject', type=int, default=3)
+    parser.add_argument("--learning_rate", dest='learning_rate', type=float, default=0.0001)
+    parser.add_argument("--latent_dim", dest='latent_dim', type=int, default=30)
+    parser.add_argument("--batch_size", dest='batch_size', type=int, default=30)
+    parser.add_argument("--num_epochs", dest='num_epochs', type=int, default=15)
+    parser.add_argument("--roi_class", dest='roi_class', default='floc-bodies') # floc-bodies, ...
+    parser.add_argument("--hem", dest='hem', default='lh') # lh, rh, all
+    parser.add_argument("--sparsity", dest='sparsity', type=float, default=0.8)
+    parser.add_argument("--l1", dest='l1', type=float, default=0.1)
+    parser.add_argument("--subject", dest='subject', type=int, default=3)
+    parser.add_argument("--img", dest='img', type=int, default=0)
 
-    usr_args, default_args = OmegaConf.from_argparse(parser)
+    config = vars(parser.parse_args())
+
+    # usr_args, default_args = OmegaConf.from_argparse(parser)
+    # config = OmegaConf.merge(usr_args, default_args)
 
     # create the results folder
     # results_folder = f'{env_config.RESULTS_DIR}/subj{usr_args.config.subject}/latent{usr_args.config.latent_dim}_sparsity{usr_args.config.sparsity}_bs{usr_args.config.batch_size}_lOne{usr_args.config.l1}'
@@ -47,7 +50,7 @@ def main(argv):
     #     for key, value in usr_args.config.items():
     #         f.write(f'{key}:{value}\n')
 
-    train.train_and_evaluate(default_args.config if usr_args == None else usr_args.config, env_config)
+    train.train_and_evaluate(config, env_config)
 
 
 if __name__ == '__main__':
